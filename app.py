1d8c615cd6699d8f99e6cc49be6a177b8b3d0060
Sopanat Pongmarutai <tamsopanat123@gmail.com>
2026-05-20 16:13:55 +0700

    Fix host

--- app.py
+++ app.py
@@ -606,4 +606,4 @@ def get_tambons(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
